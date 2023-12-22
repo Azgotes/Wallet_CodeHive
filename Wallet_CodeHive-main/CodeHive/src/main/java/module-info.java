7a4202507cd56f45285f99687e1c1b3f4339a2c6
EnzoDEPRE <146435767+EnzoDEPRE@users.noreply.github.com>
2023-12-22 11:05:39 +0100

    Delete Wallet_CodeHive-main directory

--- Wallet_CodeHive-main/CodeHive/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.exemple.codehive {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.exemple.codehive to javafx.fxml;
-    exports com.exemple.codehive;
-}
--- Wallet_CodeHive-main/README.md
+++
@@ -1,1 +0,0 @@
-![MicrosoftTeams-image](https://github.com/Azgotes/Wallet_CodeHive/assets/146435767/8d50bc6d-f7e7-4368-831e-aa9e195d1cd9)
